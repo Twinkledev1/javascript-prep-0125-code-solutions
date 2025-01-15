f35a4b542f3f414dcb49cdb8734ea15ada8d75d4
Twinkledev1 <twinkledevwanshi93@gmail.com>
2025-01-15 12:09:16 -0800

    completed prep-javascript-variables exercise

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,22 @@
+num = 5+7
+   console.log("sum of both number is" =, num);
+   console.log("type of sum:",num)
+
+name = Twinkle
+last_name = Devwanshi
+   console.log("Twinkle" + "Devwanshi") as fullName;
+
+const sum = 4+3;
+const fullName = ('Twinkle' + 'Devwanshi');
+const result = 8 + 'Banana';
+
+
+const isGreater = 10 > 5;
+const isEqual = 5 === "5";
+const isLesser = 9<40 ;
+const isNotequal = 20 !== 20
+
+
+let y = 8;
+y += 6; // same as y = y + 6;
+console.log(y); // 14
